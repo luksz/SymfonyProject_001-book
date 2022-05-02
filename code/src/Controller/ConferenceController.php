8e238b11acd52b7816cc712e4b199bce9a5f84de
Łukasz Zychal <lukaszzpriv@gmail.com>
2022-05-02 23:12:57 +0200

    add slug

--- code/src/Controller/ConferenceController.php
+++ code/src/Controller/ConferenceController.php
@@ -22,7 +22,7 @@ class ConferenceController extends AbstractController
 
     }
 
-    #[Route('/conference/{id}', name: 'conference')]
+    #[Route('/conference/{slug}', name: 'conference')]
         public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
         {
             $offset = max(0,$request->query->get('offset',0));
