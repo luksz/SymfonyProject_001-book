6bd5ac86af3d7c4fec498951df29fdcfe85390ec
Łukasz Zychal <lukaszzpriv@gmail.com>
2022-05-07 13:08:28 +0200

    add test conferenace page

--- tests/ConferenceControllerTest.php
+++ tests/ConferenceControllerTest.php
@@ -14,4 +14,22 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback!');
     }
+
+    public function testConferencePage()
+        {
+                $client = static::createClient();
+                $crawler = $client->request('GET', '/conference');
+
+                $this->assertCount(2, $crawler->filter('h4'));
+
+                $client->click($crawler->filter('h4 + p a')->link());
+
+                $this->assertResponseIsSuccessful();
+                $this->assertPageTitleContains('Amsterdam');
+                $this->assertSelectorTextContains('h2','Amsterdam');
+                $this->assertSelectorExists('div:contains("There are 1 comments.")');
+
+
+        }
+
 }
