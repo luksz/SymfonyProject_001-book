b914b9b6307c89452963167ff0dc709db09136f9
Łukasz Zychal <lukaszzpriv@gmail.com>
2022-05-07 14:17:55 +0200

    Add fuctional tests and reset database each test

--- tests/ConferenceControllerTest.php
+++ tests/ConferenceControllerTest.php
@@ -15,8 +15,27 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h2', 'Give your feedback!');
     }
 
+    
+    public function  testCommentSend()
+    {
+            $client = static::createClient();
+            $crawler = $client->request('GET', '/conference/amsterdam-2019');
+            $client->submitForm('Submit',[
+                'comment_form[author]' => 'Angelika',
+                'comment_form[text]' => 'Komentarz z testu atomatycznego',
+                'comment_form[email]' => 'test@test.pl',
+                'comment_form[photo]' => dirname(__DIR__,2).'/public/images/under-construction.gif'
+            ]);
+            $this->assertResponseRedirects();
+            $client->followRedirect();
+            $this->assertSelectorExists('div:contains("There are 2 comments.")');
+
+            
+    }
+
+    
     public function testConferencePage()
-        {
+    {
                 $client = static::createClient();
                 $crawler = $client->request('GET', '/conference');
 
@@ -30,6 +49,7 @@ class ConferenceControllerTest extends WebTestCase
                 $this->assertSelectorExists('div:contains("There are 1 comments.")');
 
 
-        }
+    }
 
+    
 }
